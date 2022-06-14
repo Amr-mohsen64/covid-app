0221db9d67986eb58ae0be3caee607666aad5f64
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-06-15 01:31:38 +0200

    Enhance qr code

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,49 +1,101 @@
 import React, { useEffect, useState } from "react";
 import QRCode from "qrcode";
+import { db } from "../firebase";
+import { useAuth } from "../contexts/AuthContext";
 
 export const Dashboard = () => {
   const [src, setSrc] = useState("");
+  const [userData, setUserData] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { currentUser } = useAuth();
+
   useEffect(() => {
-    QRCode.toDataURL(
-      `
-      name : amr mohsen 
-      age:24 ,
-      id:55454554
-    
-    `
-    ).then((data) => {
-      setSrc(data);
-    });
+    if (userData != null) {
+      QRCode.toDataURL(
+        `
+          Email: ${userData.email}
+          Name: ${userData.fullName}
+          National Id: ${userData.name}
+          Gender: ${userData.gender}
+        `
+      ).then((data) => {
+        setSrc(data);
+      });
+    }
   }, []);
 
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  async function getUserData() {
+    var docRef = db.collection("users").doc(currentUser.uid);
+    setIsLoading(true);
+    docRef
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setUserData(doc.data());
+          setIsLoading(false);
+        } else {
+          setUserData({});
+          setIsLoading(false);
+        }
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        alert(error);
+      });
+  }
+
   return (
-    <div style={{ width: "45rem" }}>
-      <img src={src} alt="qrcode" className="m-auto" />
-      <div className="card mt-3 fw-bold" style={{ width: "100%" }}>
-        <h2 className="card-header text-secondary">Your Infomation</h2>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            Email :
-            <span className="d-inline-block ms-2 text-primary">
-              amrmohsen72@gmail.com
-            </span>
-          </li>
-          <li className="list-group-item">
-            Full Name :
-            <span className="d-inline-block ms-2 text-primary">Amr Mohsen</span>
-          </li>
-          <li className="list-group-item">
-            Gender :
-            <span className="d-inline-block ms-2 text-primary">Male</span>
-          </li>
-          <li className="list-group-item">
-            nationa lId :
-            <span className="d-inline-block ms-2 text-primary">
-              29807060103895
-            </span>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <>
+      {!isLoading && (
+        <div style={{ width: "45rem" }}>
+          <div className="w-50 m-auto">
+            <img src={src} alt="qrcode" className="w-100 " />
+          </div>
+          <div className="card mt-3 fw-bold" style={{ width: "100%" }}>
+            <h2 className="card-header text-secondary">Your Infomation</h2>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                Email :
+                <span className="d-inline-block ms-2 text-primary">
+                  {userData.email}
+                </span>
+              </li>
+              <li className="list-group-item">
+                Full Name :
+                <span className="d-inline-block ms-2 text-primary">
+                  {userData.fullName}
+                </span>
+              </li>
+              <li className="list-group-item">
+                Gender :
+                <span className="d-inline-block ms-2 text-primary">
+                  {userData.gender}
+                </span>
+              </li>
+              <li className="list-group-item">
+                nationa lId :
+                <span className="d-inline-block ms-2 text-primary">
+                  {userData.nationalId}
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+      {isLoading && (
+        <div
+          className="spinner-grow text-light d-flex justify-content-center"
+          role="status"
+          style={{ width: "218px", height: "218px" }}
+        >
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      )}
+    </>
   );
 };
