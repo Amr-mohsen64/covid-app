62f51f74c8ee206611730c32ddd0e98fa4507cb2
Amr Mohsen <amrmohsendev@gmail.com>
2022-06-14 22:31:42 +0200

    use useInput hook

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useInput from "../hooks/use-input"
+
+const isEmail = value => value.includes("@");
 
 const SignUp = () => {
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    InputBlurHandler: emailBLurHandler,
+    reset: restEmail
+  } = useInput(isEmail)
+
+  console.log(emailValue);
   return (
     <div className="card" style={{ width: "30rem" }}>
       <div className="card-body">
@@ -15,6 +28,7 @@ const SignUp = () => {
               type="email"
               className="form-control"
               id="exampleInputEmail1"
+              onChange={emailChangeHandler}
             />
             <div id="emailHelp" className="form-text">
               We'll never share your email with anyone else.
