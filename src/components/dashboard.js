6a84b3a856cca60f53c2f927e4b8f1231cc0517e
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-06-15 03:32:37 +0200

    render according to is reserved

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 import QRCode from "qrcode";
 import { db } from "../firebase";
 import { useAuth } from "../contexts/AuthContext";
+import useInput from "../hooks/use-input";
 
 export const Dashboard = () => {
   const [src, setSrc] = useState("");
@@ -17,11 +18,24 @@ export const Dashboard = () => {
         Name: ${userData.fullName}
         National Id: ${userData.nationalId}
         Gender: ${userData.gender}
+        Vacine : ${userData.vacine}
+        First Dose  : ${userData.firstDose}
+        Second Dose  : ${userData.secondDose}
+        Third Dose  : ${userData.thirdDose}
       `
     ).then((data) => {
       setSrc(data);
     });
-  }, [userData.email, userData.fullName, userData.gender, userData.nationalId]);
+  }, [
+    userData.email,
+    userData.firstDose,
+    userData.fullName,
+    userData.gender,
+    userData.nationalId,
+    userData.secondDose,
+    userData.thirdDose,
+    userData.vacine,
+  ]);
 
   const getUserData = useCallback(() => {
     var docRef = db.collection("users").doc(currentUser.uid);
@@ -52,6 +66,74 @@ export const Dashboard = () => {
     setFormIsShown((prevState) => !prevState);
   }
 
+  const isNotEmpty = (value) => value.trim() !== "";
+
+  const {
+    value: vacineValue,
+    isValid: vacineIsValid,
+    hasError: vacineHasError,
+    valueChangeHandler: vacineChangeHandler,
+    InputBlurHandler: vacineBLurHandler,
+    reset: restVacine,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: firstDoseValue,
+    isValid: firstDoseIsValid,
+    hasError: firstDoseHasError,
+    valueChangeHandler: firstDoseChangeHandler,
+    InputBlurHandler: firstDoseBLurHandler,
+    reset: restFirstDose,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: secondDoseValue,
+    isValid: secondDoseIsValid,
+    hasError: secondDoseHasError,
+    valueChangeHandler: secondDoseChangeHandler,
+    InputBlurHandler: secondDoseBLurHandler,
+    reset: restSecondDose,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: thirdDoseValue,
+    isValid: thirdDoseIsValid,
+    hasError: thirdDoseHasError,
+    valueChangeHandler: thirdDoseChangeHandler,
+    InputBlurHandler: thirdDoseBLurHandler,
+    reset: restThirdDose,
+  } = useInput(isNotEmpty);
+
+  let formIsValid = false;
+  if (
+    vacineIsValid &&
+    firstDoseIsValid &&
+    secondDoseIsValid &&
+    thirdDoseIsValid
+  ) {
+    formIsValid = true;
+  }
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    console.log(vacineValue, firstDoseValue, secondDoseValue, thirdDoseValue);
+
+    db.collection("users")
+      .doc(currentUser.uid)
+      .set(
+        {
+          vacine: vacineValue,
+          firstDose: firstDoseValue,
+          secondDose: secondDoseValue,
+          thirdDose: thirdDoseValue,
+          isReserved: true,
+        },
+        { merge: true }
+      )
+      .then((d) => console.log(d));
+  };
+
+  // console.log(userData);
   return (
     <>
       {!isLoading && (
@@ -81,33 +163,69 @@ export const Dashboard = () => {
                 </span>
               </li>
               <li className="list-group-item">
+                vacine :
+                <span className="d-inline-block ms-2 text-primary">
+                  {userData.vacine}
+                </span>
+              </li>
+
+              <li className="list-group-item">
+                first Dose :
+                <span className="d-inline-block ms-2 text-primary">
+                  {userData.firstDose}
+                </span>
+              </li>
+              <li className="list-group-item">
                 nationa lId :
                 <span className="d-inline-block ms-2 text-primary">
                   {userData.nationalId}
                 </span>
               </li>
               <li className="list-group-item">
-                <button
-                  className="btn btn-danger"
-                  onClick={handleVacFormApperance}
-                >
-                  Set Vacination Date
-                </button>
+                second Dose:
+                <span className="d-inline-block ms-2 text-primary">
+                  {userData.secondDose}
+                </span>
               </li>
+              <li className="list-group-item">
+                third Dose :
+                <span className="d-inline-block ms-2 text-primary">
+                  {userData.thirdDose}
+                </span>
+              </li>
+              {!userData.isReserved && (
+                <li className="list-group-item">
+                  <button
+                    className="btn btn-danger"
+                    onClick={handleVacFormApperance}
+                  >
+                    Set Vacination Date
+                  </button>
+                </li>
+              )}
               {formIsShowen && (
                 <div className="card">
                   <div className="card-body">
-                  <h2 className="text-danger">Vacine Info</h2>
-                    <form>
+                    <h2 className="text-danger">Vacine Info</h2>
+                    <form onSubmit={submitHandler}>
                       <div className="mb-3">
                         <label htmlFor="Vaccine" className="form-label">
                           Vaccine
                         </label>
-                        <select className="form-select">
+                        <select
+                          className="form-select"
+                          onChange={vacineChangeHandler}
+                          onBlur={vacineBLurHandler}
+                        >
                           <option>Pfizer</option>
                           <option>Sinopharm </option>
                           <option>Covaxin</option>
                         </select>
+                        {vacineHasError && (
+                          <div className="form-text fw-bold text-danger">
+                            Please select vacine
+                          </div>
+                        )}
                       </div>
 
                       <div className="mb-3">
@@ -118,7 +236,14 @@ export const Dashboard = () => {
                           type="date"
                           className="form-control invalid"
                           id="firstDose"
+                          onChange={firstDoseChangeHandler}
+                          onBlur={firstDoseBLurHandler}
                         />
+                        {firstDoseHasError && (
+                          <div className="form-text fw-bold text-danger">
+                            Please select First Dose Date
+                          </div>
+                        )}
                       </div>
                       <div className="mb-3">
                         <label htmlFor="secondDose" className="form-label">
@@ -128,7 +253,14 @@ export const Dashboard = () => {
                           type="date"
                           className="form-control invalid"
                           id="secondDose"
+                          onChange={secondDoseChangeHandler}
+                          onBlur={secondDoseBLurHandler}
                         />
+                        {secondDoseHasError && (
+                          <div className="form-text fw-bold text-danger">
+                            Please select second Dose Date
+                          </div>
+                        )}
                       </div>
                       <div className="mb-3">
                         <label htmlFor="thirdDose" className="form-label">
@@ -138,8 +270,21 @@ export const Dashboard = () => {
                           type="date"
                           className="form-control invalid"
                           id="thirdDose"
+                          onChange={thirdDoseChangeHandler}
+                          onBlur={thirdDoseBLurHandler}
                         />
+                        {thirdDoseHasError && (
+                          <div className="form-text fw-bold text-danger">
+                            Please select third Dose Date
+                          </div>
+                        )}
                       </div>
+                      <button
+                        className="btn btn-primary"
+                        disabled={!formIsValid}
+                      >
+                        Submit
+                      </button>
                     </form>
                   </div>
                 </div>
